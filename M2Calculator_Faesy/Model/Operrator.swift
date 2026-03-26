c88a5e082138d028934ecca8521096042872165d
BCooperF <bradley_faesy@mymail.eku.edu>
2026-03-26 11:58:48 -0400

    M8 calc update

---
+++ M2Calculator_Faesy/Model/Operrator.swift
@@ -0,0 +1,15 @@
+//
+//  Operrator.swift
+//  M2Calculator_Faesy
+//
+//  Created by Faesy, Bradley C. on 3/26/26.
+//
+
+import Foundation
+
+enum Operrator {
+	case plus
+	case minus
+	case multiply
+	case divide
+}
--- M2Calculator_Faesy/ViewController.swift
+++ M2Calculator_Faesy/ViewController.swift
@@ -12,26 +12,33 @@ class ViewController: UIViewController {
 	@IBOutlet weak var operand1TextField: UITextField!
 	@IBOutlet weak var operand2TextField: UITextField!
 	@IBOutlet weak var selectOperator: UIButton!
+	
+	var selectedOperrator: Operrator?
+	
 	@IBAction func operatorButton(_ sender: Any) {
 		let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle:.alert)
-		
+				
 		let plusAction = UIAlertAction(title: "+(plus)", style: .default) { _ in
 			self.selectOperator.setTitle("+", for: .normal)
+			self.selectedOperrator = .plus
 		}
 		actionSheet.addAction(plusAction)
 		
 		let minusAction = UIAlertAction(title: "-(minus)", style: .default) { _ in
 			self.selectOperator.setTitle("-", for: .normal)
+			self.selectedOperrator = .minus
 		}
 		actionSheet.addAction(minusAction)
 		
 		let multiplyAction = UIAlertAction(title: "*(multiply)", style: .default) { _ in
 			self.selectOperator.setTitle("*", for: .normal)
+			self.selectedOperrator = .multiply
 		}
 		actionSheet.addAction(multiplyAction)
 		
 		let divideAction = UIAlertAction(title: "/(divide)", style: .default) { _ in
 			self.selectOperator.setTitle("/", for: .normal)
+			self.selectedOperrator = .divide
 		}
 		actionSheet.addAction(divideAction)
 		
@@ -40,33 +47,58 @@ class ViewController: UIViewController {
 	}
 	@IBOutlet weak var resultLabel: UILabel!
 	
+	func showAlert(message: String, title: String? = "Alert"){
+		let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+		let okAction = UIAlertAction(title: "OK", style: .default)
+		alert.addAction(okAction)
+		present(alert, animated: true)
+	}
+	
 	@IBAction func resultButton(_ sender: Any) {
-		let op1 = Int(operand1TextField.text!)!
-		let op2 = Int(operand2TextField.text!)!
-		let op = selectOperator.title(for: .normal)
+		guard let op1 = operand1TextField.text, let a = Int(op1) else{
+			operand1TextField.becomeFirstResponder()
+			showAlert(message: "Please enter a number.", title: "op1 Error")
+			return
+		}
+		guard let op2 = operand2TextField.text, let b = Int(op2) else{
+			operand2TextField.becomeFirstResponder()
+			showAlert(message: "Please enter a number.")
+			return
+		}
+		guard let op = selectedOperrator else{
+			showAlert(message: "Please select an operator.")
+			return
+		}
 		
-		if op == "+" {
-			let result = op1 + op2
-			resultLabel.text = "\(result)"
-		}else if op == "-" {
-			let result = op1 - op2
-			resultLabel.text = "\(result)"
-		}else if op == "*" {
-			let result = op1 * op2
-			resultLabel.text = "\(result)"
-		}else if op == "/" {
-			let result = op1 / op2
-			resultLabel.text = "\(result)"
+		var result: Int? = nil
+		switch op {
+		case .plus:
+			result = a + b
+		case .minus:
+			result = a - b
+		case .multiply:
+			result = a * b
+		case .divide:
+			result = a / b
+		}
+		guard let result = result else{
+			return
+		}
+		resultLabel.text = "\(result)"
+		
+		if operand1TextField.isFirstResponder{
+			operand1TextField.resignFirstResponder()
 		}else{
-			print("Invalid Operator")
+			operand2TextField.resignFirstResponder()
 		}
 	}
 	
-	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
 	}
-
-
+	
+	override func viewDidAppear(_ animated: Bool) {
+		super.viewWillAppear(animated)
+		operand1TextField.becomeFirstResponder( )
+	}
 }
